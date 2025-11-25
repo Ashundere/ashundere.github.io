d07f446787d6307dc70a9d720557dc2eeb928fdc
Devon Jones <devon.shun.jones@gmail.com>
2025-11-25 10:40:03 -0600

    Finished Javascript

--- scripts.js
+++ scripts.js
@@ -10,7 +10,8 @@ const longUrlToShorten = 'https://www.example.com/your/long/path/here';
 shortenBtn.addEventListener("click", event =>{
     if (urlInput.checkValidity()) {
         urlInputError.textContent = ""
-        alert("Form Submitted Successfully");//replace with shortenurl function
+        console.log(urlInput.value)
+       shortenUrl(urlInput.value)
     }else{
         urlInputError.textContent = "Please enter a url";
         urlInput.focus();
@@ -28,6 +29,17 @@ if (recall == null) {
   recall = [];
 }
 
+let objectCounter = sessionStorage.getItem('ObjectCounter');
+if (objectCounter === null) {
+    objectCounter = 0;
+} else {
+     objectCounter = parseInt(objectCounter, 10); 
+}
+
+const incObjectCounter=()=>{
+    objectCounter++
+    sessionStorage.setItem('ObjectCounter', objectCounter.toString())
+}
 async function shortenUrl(longUrl) {
     const apiUrl = 'https://api-ssl.bitly.com/v4/shorten';
     const headers = {
@@ -51,21 +63,64 @@ async function shortenUrl(longUrl) {
         }
 
         const data = await response.json();
-        console.log('Shortened URL:', data.link); 
-        return data.link;
+        console.log('Shortened URL:', data.link);
+        try {
+            createListElement(data.link)
+        } catch (error) {
+            console.error("Failed to create new list element", error)
+        } 
 
     } catch (error) {
         console.error('Error shortening URL:', error.message);
     }
 }
 
-// shortenUrl(longUrlToShorten)
 
 const createListElement =(shortenedUrl)=>{
     const element = {
+        id: objectCounter,
         longLink: urlInput.value,
         shortLink: shortenedUrl
     }
     recall.push(element);
-    sessionStorage.setItem("savedUrls", JSON.stringify(recall));    
+    sessionStorage.setItem("savedUrls", JSON.stringify(recall))
+    const newListElement = document.createElement("div")
+    newListElement.setAttribute("id", element.id)
+    const ElementIndex = recall.findIndex(
+    (element) => element.id == newListElement.id
+  );
+    newListElement.innerHTML = `${recall[ElementIndex].longLink}          ${recall[ElementIndex].shortLink} <button class = "copy">Copy</button>`    
+    urlList.appendChild(newListElement)
+    incObjectCounter()
+    console.log(recall)
 }
+
+const recollection = () => {
+  for (let i = 0; i <= recall.length; i++) {
+    const newListElement = document.createElement("div");
+    newListElement.setAttribute("id", recall[i].id);
+    newListElement.innerHTML = `${recall[i].longLink}          ${recall[i].shortLink} <button class = "copy">Copy</button>`
+    urlList.appendChild(newListElement);
+}}
+
+
+window.addEventListener("load", (event) => {
+  recollection();
+});
+
+async function copyClipboard(text){
+    try {
+        await navigator.clipboard.writeText(text)
+    } catch (error) {
+        console.error("Error copying to clipboard", error)
+    }
+}
+
+urlList.addEventListener("click", event =>{
+    if(event.target.classList.contains("copy")){
+        urlId = event.target.closest("div").id
+        copyClipboard(`${recall[urlId].shortLink}`)
+        event.target.innerText = "Copied!"
+        event.target.style.backgroundColor = "hsl(257, 27%, 26%)";
+    }
+})
